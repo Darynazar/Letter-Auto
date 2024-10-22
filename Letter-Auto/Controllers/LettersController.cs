ec2ecf15839c8e5d526a68261f347dd95cefe418
darynazar <heydaro.std00@gmail.com>
2024-10-22 12:17:06 +0330

    add letter controller and views

---
+++ Letter-Auto/Controllers/LettersController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Letter_Auto.Data;
+using Letter_Auto.Models;
+
+namespace Letter_Auto.Controllers
+{
+    public class LettersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LettersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Letters
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Letters.Include(l => l.Category).Include(l => l.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Letters/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var letter = await _context.Letters
+                .Include(l => l.Category)
+                .Include(l => l.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (letter == null)
+            {
+                return NotFound();
+            }
+
+            return View(letter);
+        }
+
+        // GET: Letters/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Letters/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Subject,Sender,Receiver,Description,Status,CurrentOrganization,CategoryId,UserId,Image")] Letter letter)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(letter);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", letter.CategoryId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", letter.UserId);
+            return View(letter);
+        }
+
+        // GET: Letters/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var letter = await _context.Letters.FindAsync(id);
+            if (letter == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", letter.CategoryId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", letter.UserId);
+            return View(letter);
+        }
+
+        // POST: Letters/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Subject,Sender,Receiver,Description,Status,CurrentOrganization,CategoryId,UserId,Image")] Letter letter)
+        {
+            if (id != letter.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(letter);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LetterExists(letter.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", letter.CategoryId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", letter.UserId);
+            return View(letter);
+        }
+
+        // GET: Letters/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var letter = await _context.Letters
+                .Include(l => l.Category)
+                .Include(l => l.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (letter == null)
+            {
+                return NotFound();
+            }
+
+            return View(letter);
+        }
+
+        // POST: Letters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var letter = await _context.Letters.FindAsync(id);
+            if (letter != null)
+            {
+                _context.Letters.Remove(letter);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LetterExists(int id)
+        {
+            return _context.Letters.Any(e => e.Id == id);
+        }
+    }
+}
